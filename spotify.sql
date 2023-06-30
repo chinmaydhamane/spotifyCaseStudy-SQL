a785c6ccb0d7cde53d30c88461ebf0655f7fe537
chinmaydhamane <138246702+chinmaydhamane@users.noreply.github.com>
2023-07-01 00:49:43 +0530

    Add files via upload

---
+++ spotify.sql
@@ -0,0 +1,67 @@
+CREATE EXTERNAL TABLE activity5 (
+  user_id INT,
+  event_name VARCHAR(13),
+  event_date DATE,
+  country VARCHAR(20)
+)
+LOCATION 's3://credit-input/'
+;
+
+
+INSERT INTO activity5 VALUES
+  (1, 'app-installed', DATE '2022-01-01', 'India'),
+  (1, 'app-purchase', DATE '2022-01-02', 'India'),
+  (2, 'app-installed', DATE '2022-01-01', 'USA'),
+  (3, 'app-installed', DATE '2022-01-01', 'USA'),
+  (3, 'app-purchase', DATE '2022-01-03', 'USA'),
+  (4, 'app-installed', DATE '2022-01-03', 'India'),
+  (4, 'app-purchase', DATE '2022-01-03', 'India'),
+  (5, 'app-installed', DATE '2022-01-03', 'SL'),
+  (5, 'app-purchase', DATE '2022-01-03', 'SL'),
+  (6, 'app-installed', DATE '2022-01-04', 'Pakistan'),
+  (6, 'app-purchase', DATE '2022-01-04', 'Pakistan');
+
+
+
+-- 1. Find total active users each day.
+select event_date, COUNT(DISTINCT user_id) as daily_users 
+FROM activity5
+group by event_date
+ORDER BY event_date;
+
+-- 2. Find total active users each week. 
+SELECT WEEK(event_date) AS event_week, COUNT(DISTINCT user_id) AS weekly_users
+FROM activity5
+GROUP BY WEEK(event_date);
+
+-- 3. Date wise total number of users who made the purchase same day they installed the app.
+with cte as (
+select user_id, event_date,
+CASE WHEN COUNT(DISTINCT event_name)=2 THEN user_id ELSE NULL end as sameday_user
+FROM activity5
+GROUP BY user_id,event_date
+)
+
+SELECT event_date, count(sameday_user)
+FROM cte
+GROUP BY event_date
+ORDER BY event_date;
+
+
+-- 4. Percentage of Paid Users in India, USA and any other country should be tagged as others.
+with cte1 as (
+select CASE WHEN country NOT IN ('India', 'USA') THEN 'others' ELSE country END AS country,
+count(DISTINCT user_id) as total_users_countrywise
+FROM activity5
+WHERE event_name = 'app-purchase'
+GROUP BY CASE WHEN country NOT IN('India', 'USA') THEN 'others' ELSE country END
+),
+
+cte2 as (
+select sum(total_users_countrywise) as total_paid_users
+FROM cte1
+)
+
+SELECT cte1.Country, CAST((cte1.total_users_countrywise * 1.0 / cte2.total_paid_users * 100) as int) as percent_paid
+FROM cte1 JOIN cte2 ON 1=1;
+
